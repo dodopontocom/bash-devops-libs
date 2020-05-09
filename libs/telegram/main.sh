0dbae756002927216268dfde92e2f55b098baacc
rodolfotiago <rodolfotiago@gmail.com>
2020-05-09 09:28:42 -0300

    message variable

--- libs/telegram/main.sh
+++ libs/telegram/main.sh
@@ -8,19 +8,19 @@ checkVars TELEGRAM_BOT_TOKEN TELEGRAM_NOTIFICATION_ID || return ${?}
 # @description Check Telegram documentation to [Create a bot and Generating an authorization token](https://core.telegram.org/bots#6-botfather)
 # @arg $TELEGRAM_BOT_TOKEN telegram bot token
 # @arg $TELEGRAM_NOTIFICATION_ID user receiver telegram id
-# @arg $TELEGRAM_MESSAGE message content
+# @arg $message message content
 # @exitcode 0 Message is sent
 # @exitcode 1 Error to send the message
 # @example
-#   sendMessage <TELEGRAM_BOT_TOKEN> <TELEGRAM_NOTIFICATION_ID> <TELEGRAM_MESSAGE>
+#   sendMessage <TELEGRAM_BOT_TOKEN> <TELEGRAM_NOTIFICATION_ID> <message>
 function sendMessage() {
 
-    getArgs "TELEGRAM_MESSAGE"
+    getArgs "message"
     
     # Send Telegram Notification
     curl -X POST \
                 -d chat_id=${TELEGRAM_NOTIFICATION_ID} \
-                -d text="${TELEGRAM_MESSAGE}" \
+                -d text="${message}" \
                 https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage &> /dev/null
     exitOnError "Error while trying to use telegram api to send the message."
 
